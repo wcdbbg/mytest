18895a9766b48a87ec77a1c014592deb0ee761e3
wangc <wcdbbg@sina.com>
2018-12-18 15:02:46 +0800

    我的项目

---
+++ src/Student.java
@@ -0,0 +1,12 @@
+/**
+ * Author:     WangChao
+ * Date:       2018/12/17 12:29
+ * Description:学生实体类
+ */
+
+public class Student {
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
---
+++ src/Theacher.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright ©2018,上海七圣网络科技有限公司
+ * Author:     WangChao
+ * Date:       2018/12/17 12:37
+ * Description:老师实体类
+ */
+
+public class Theacher {
+
+
+    public static void main(String[] args) {
+        /**
+         * 功能描述:
+         *
+         * @param args
+         * @return: void
+         * @Author: WangChao
+         * @Date:   2018/12/17 13:49
+         */
+    }
+
+
+    public User getUser(String id, String name) {
+
+
+        return null;
+    }
+}
